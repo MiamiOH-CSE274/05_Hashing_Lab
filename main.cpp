8e54f3ce584e2a97e081d9c25c9033395e686a80
Bo Brinkman <Bo.Brinkman@MiamiOH.edu>
2013-07-25 14:05:42 -0700

    Added tester methods, and list of good hash primes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
+#include "hashPrimes.h"
 #include "HashTable.h"
 #include <string>
 #include <iostream>
+#include <sstream>
+
+/**
+ * This is a hash function for strings, based on Horner's method. See
+ * http://en.wikipedia.org/wiki/Horner's_method
+ *
+ * If you want to use the HashTable class with a different Key type, then
+ * you would need to overload the hash() function, creating a version
+ * with the appropriate input type.
+ */
+unsigned long hash(std::string k){
+  unsigned long m = hashPrimes[NUM_HASH_PRIMES-2];
+  unsigned long ret = 0;
+  for(int i=0;i<k.size();i++){
+    ret = (256*ret + k[i])%m;
+  }
+  return ret;
+}
 
 //Check to make sure that numItems returns correct result on a fresh
 // instance of the LinkedList class
@@ -10,88 +29,61 @@ void testCtor(){
     std::cout << "SUCCESS: Fresh hash table has 0 items" << std::endl;
   } else {
     std::cout << "ERROR: Fresh hash table should have 0 items, but has " << testTable.size() << std::endl;
+    return;
   }
 }
 
 //Add and remove some items, making sure they come back in the
 // correct order
 void testBasicMethods(){
-  /*  HashTable<string,int> testList;
-  testList.add(0,0);
-  testList.add(0,1);
-  testList.add(0,2);
-  testList.add(3,3);
-  testList.add(4,4);
-  testList.add(5,5);
+  HashTable<std::string,int> testHash;
+  testHash.add("how",0);
+  testHash.add("now",1);
+  testHash.add("brown",2);
+  testHash.add("cow",3);
+  testHash.add("metal",4);
+  testHash.add("daffodil",5);
 
-  if(testList.size() == 6){
+  if(testHash.size() == 6){
     std::cout << "SUCCESS: 6 items added" << std::endl;
   } else {
-    std::cout << "ERROR: Added 6 items, but size says " << testList.size() << std::endl;
+    std::cout << "ERROR: Added 6 items, but size says " << testHash.size() << std::endl;
+    return;
+  }
+  
+  int x0 = testHash.find("how");
+  int x1 = testHash.find("now");
+  int x2 = testHash.find("brown");
+  int x3 = testHash.find("cow");
+  int x4 = testHash.find("metal");
+  int x5 = testHash.find("daffodil");
+
+  if(x0 != 0 || x1 != 1 || x2 != 2 || x3 != 3 || x4 != 4 || x5 != 5){
+    std::cout << "ERROR: Expected 0,1,2,3,4,5, but got " << x0 <<"," << x1 << "," << x2 << "," << x3 << "," << x4 << "," << x5 << std::endl;
+    return;
+  } else {
+    std::cout << "SUCCESS: 6 added items came back out with correct keys" << std::endl;
+  }
+
+  if(testHash.keyExists("daffodil")){
+    std::cout << "SUCCESS: keyExists found 'daffodil'" << std::endl;
+  } else {
+    std::cout << "ERROR: 'daffodil' is a valid key, but keyExists said false" << std::endl;
     return;
   }
 
-  int x0 = testList.get(0);
-  int x1 = testList.get(1);
-  int x2 = testList.get(2);
-  int x3 = testList.get(3);
-  int x4 = testList.get(4);
-  int x5 = testList.get(5);
-
-  if(x0 != 2 || x1 != 1 || x2 != 0 || x3 != 3 || x4 != 4 || x5 != 5){
-    std::cout << "ERROR: Expected 2,1,0,3,4,5, but got " << x0 <<"," << x1 << "," << x2 << "," << x3 << "," << x4 << "," << x5 << std::endl;
+  if(!testHash.keyExists("shiny")){
+    std::cout << "SUCCESS: keyExists did not find 'shiny'" << std::endl;
   } else {
-    std::cout << "SUCCESS: 6 added items came back out in the correct order" << std::endl;
-  }
-
-  testList.remove(5);
-  testList.remove(0);
-
-  if(testList.size() == 4){
-    std::cout << "SUCCESS: 4 items after removing" << std::endl;
-  } else {
-    std::cout << "ERROR: After remove, expected 4 items, but size says " << testList.size() << std::endl;
+    std::cout << "ERROR: 'shiny' is not a valid key, but keyExists said true" << std::endl;
     return;
   }
 
-  x0 = testList.get(0);
-  x1 = testList.get(1);
-  x2 = testList.get(2);
-  x3 = testList.get(3);
-
-  if(x0 != 1 || x1 != 0 || x2 != 3 || x3 != 4){
-    std::cout << "ERROR: After remove, expected 1,0,3,4, but got " << x0 <<"," << x1 << "," << x2 << "," << x3 << std::endl;
-  } else {
-    std::cout << "SUCCESS: After remove, 4 items came back out in the correct order" << std::endl;
-  }
-
-  testList.set(0,14);
-  testList.set(1,13);
-  testList.set(2,12);
-  testList.set(3,11);
-  if(testList.size() == 4){
-    std::cout << "SUCCESS: List size not changed after set" << std::endl;
-  } else {
-    std::cout << "ERROR: After set, expected 4 items, but size says " << testList.size() << std::endl;
-    return;
-  }
-
-  x0 = testList.get(0);
-  x1 = testList.get(1);
-  x2 = testList.get(2);
-  x3 = testList.get(3);
-  if(x0 != 14 || x1 != 13 || x2 != 12 || x3 != 11){
-    std::cout << "ERROR: After set, expected 14,13,12,11, but got " << x0 <<"," << x1 << "," << x2 << "," << x3 << std::endl;
-  } else {
-    std::cout << "SUCCESS: After set, 4 items came back out in the correct order" << std::endl;
-    }*/
-}
-
-void testExceptions(){
-  /*  HashTable<string,int> testList;
   bool didException = false;
   try {
-    testList.remove(0);
+
+    int xq = testHash.find("shiny");
+
   } catch (std::string s) {
     std::cout << "SUCCESS: Caught exception: " << s << std::endl;
     didException = true;
@@ -99,68 +91,93 @@ void testExceptions(){
     std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
     return;
   }
-
-  if(!didException){
-    std::cout << "ERROR: Tried to remove from an empty list, but did not get an exception" << std::endl;
-  }
-
-  testList.add(0,0);
-  testList.add(0,1);
-  testList.add(0,2);
-
-  didException = false;
-  try {
-    testList.set(5,5);
-  } catch (std::string s) {
-    std::cout << "SUCCESS: Caught exception: " << s << std::endl;
-    didException = true;
-  } catch (...) {
-    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+  if(didException == false){
+    std::cout << "ERROR: find did not throw an exception when given a non-existen key" << std::endl;
     return;
   }
 
-  if(!didException){
-    std::cout << "ERROR: Tried to set a non-existent item, but did not get an exception" << std::endl;
-  }
+  testHash.remove("how");
+  testHash.remove("now");
 
-  didException = false;
-  try {
-    int t = testList.get(5);
-  } catch (std::string s) {
-    std::cout << "SUCCESS: Caught exception: " << s << std::endl;
-    didException = true;
-  } catch (...) {
-    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+  if(testHash.size() == 4){
+    std::cout << "SUCCESS: 4 items after removing" << std::endl;
+  } else {
+    std::cout << "ERROR: After remove, expected 4 items, but size says " << testHash.size() << std::endl;
     return;
   }
 
-  if(!didException){
-    std::cout << "ERROR: Tried to get a non-existent item, but did not get an exception" << std::endl;
-  }
-
-  didException = false;
-  try {
-    testList.add(5,5);
-  } catch (std::string s) {
-    std::cout << "SUCCESS: Caught exception: " << s << std::endl;
-    didException = true;
-  } catch (...) {
-    std::cout << "ERROR: Caught an exception, but it wasn't a string type" << std::endl;
+  if(testHash.numRemoved == 2){
+    std::cout << "SUCCESS: numRemoved is 2, as expected" << std::endl;
+  } else {
+    std::cout << "ERROR: After remove, expected numRemoved == 2, but got " << testHash.numRemoved << std::endl;
     return;
   }
 
-  if(!didException){
-    std::cout << "ERROR: Tried to add at invalid index, but did not get an exception" << std::endl;
+  if(testHash.keyExists("how") == false){
+    std::cout << "SUCCESS: Removed key 'how' is no longer in hash table." << std::endl;
+  } else {
+    std::cout << "ERROR: Removed key 'how' is still in the table" << std::endl;
+    return;
   }
-  */
 
+  if(testHash.keyExists("brown") && testHash.keyExists("cow") &&
+       testHash.keyExists("metal") && testHash.keyExists("daffodil")){
+    std::cout << "SUCCESS: The 4 items that weren't removed are still in the table." << std::endl;
+  } else {
+    std::cout << "ERROR: One of the 4 keys that ought to still be in the table has been lost" << std::endl;
+    return;
+  }
+}
+
+void testGrow(){
+  HashTable<std::string,int> testHash;
+  testHash.add("0",0);
+  unsigned long startingSize = testHash.backingArraySize;
+  unsigned long i=0;
+  while(testHash.backingArraySize == startingSize){
+    i++;
+    std::ostringstream ss;
+    ss << i;
+    testHash.add(ss.str(),i);
+  }
+  unsigned long endingSize = testHash.backingArraySize;
+  if(i*2 > startingSize){
+    std::cout << "ERROR: You didn't grown soon enough. Starting size was " << startingSize << ", didn't grow until " << i+1 << "adds done." << std::endl;
+    return;
+  } else {
+    std::cout << "SUCCESS: Grow called at the right time. Starting size was " << startingSize << " and grow called on the " << i+1 << "th add call" << std::endl;
+  }
+
+  if(testHash.size() == i+1){
+    std::cout << "SUCCESS: Size of hash table unchanged by the grow" << std::endl;
+  } else {
+    std::cout << "ERROR: Size of hash table should be " << i+1 << ", but got " << testHash.size() << std::endl;
+    return;
+  }
+
+  if(testHash.numRemoved == 0){
+    std::cout << "SUCCESS: After grow called, all removed items have been cleared out." << std::endl;
+  } else {
+    std::cout << "ERROR: When grow is called, you should clear out all the removed items." << std::endl;
+    return;
+  }
+
+  for(int j=0; j<=i;j++){
+    std::ostringstream ss;
+    ss << j;
+    if(!testHash.keyExists(ss.str())){
+      std::cout << "ERROR: During grow the item with key '" << j << "' was lost" << std::endl;
+      return;
+    }
+  }
+  std::cout << "SUCCESS: After grow all items from the original table made it to the new one." << std::endl;
 }
 
 //A simple main function which creates a list, and tests it.
 int main(){
   testCtor();
   testBasicMethods();
-  testExceptions();
+  testGrow();
 
   return 0;
 }
